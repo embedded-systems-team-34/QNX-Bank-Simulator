af83e3af09d059d7be6f403e680fdce4f8a01929
dmacintyre <djm4912@rit.edu>
2017-04-01 22:37:58 -0400

    Timers Proof of Concept on QNX,
    Use actions and signals.  DO NOT create new treads

--- main.c
+++ main.c
@@ -1,70 +1,89 @@
-/*******************************************************************************
-* main.c
-* 3/7/2017
-* Donald MacIntyre - djm4912
-*
-* Description:
-*******************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
 
-/* Hello World program */
+#include <sys/neutrino.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <hw/inout.h>
 
-#include<stdio.h>
-#include <unistd.h>
-#include <semaphore.h>
-
-sem_t semaphore;
-pthread_t my_thread1;
-pthread_t my_thread2;
-pthread_t my_thread3;
+#define a1_MS (1000000)
+#define a10_MS (10000000)
+#define a100_MS (100000000)
 
 
-void handleOne() {
-    while (1) {
-        sem_wait(&semaphore);
-        printf("Executing handleOne()\n");
-        sleep(3);
-        printf("Done processing 1\n");
-    }
+uintptr_t port_a;
+uintptr_t dir;
+
+void print(){
+	static unsigned int val = 0;
+	val ^= 1;
+	out8(port_a, val);
 }
 
-void handleTwo() {
-    while (1) {
-        sem_wait(&semaphore);
-        printf("Executing handleTwo()\n");
-        sleep(2);
-        printf("Done processing 2\n");
+int main(int argc, char *argv[]) {
+	//What to do when the timer expires
+	struct sigevent event;
+	struct sigaction action;
+
+	//Parameters of the timer
+	struct itimerspec timerSpec;
+	timer_t timerID;
+
+	//Amount of time to pass before the timer will expire
+	//seconds
+	timerSpec.it_value.tv_sec = 0;
+	//Nanoseconds
+	timerSpec.it_value.tv_nsec = 500000000;
+
+	//Define the period of timer
+	timerSpec.it_interval.tv_sec = 0;
+	timerSpec.it_interval.tv_nsec = 100000;
+
+	struct _clockperiod clkper;
+
+	//Event, method address, method parameter, code
+
+	// Set clock to a period of 10 us
+	clkper.nsec       = 10000;
+	clkper.fract      = 0;
+	ClockPeriod ( CLOCK_REALTIME, &clkper, NULL, 0  );
+
+	struct timespec res;
+
+
+	action.sa_sigaction = print;
+	action.sa_flags = SA_SIGINFO;
+	sigaction( SIGUSR1, &action, NULL );
+	SIGEV_SIGNAL_INIT(&event, SIGUSR1);
+
+
+	clock_getres( CLOCK_REALTIME, &res);
+
+	printf( "Resolution is %ld micro seconds.\n",
+	          res.tv_nsec / 1000 );
+
+	timer_create(CLOCK_REALTIME , &event, &timerID);
+	timer_settime(timerID, 0, &timerSpec, NULL);
+
+    if ( ThreadCtl(_NTO_TCTL_IO, NULL) == -1)
+    {
+
+    	printf("Failed to get I/O access permission");
     }
-}
 
-void handleThree() {
-    while (1) {
-        sem_wait(&semaphore);
-        printf("Executing handleThree()\n");
-        sleep(1);
-        printf("Done processing 3\n");
-    }
-}
+    port_a = mmap_device_io(1, 0x280 + 8);
+    dir = mmap_device_io(1, 0x280 + 11);
 
-void main()
-{    
+    out8(dir,0x00);
+    //out8(port_a,1);
 
-    int num;
-    
-    srand(time(NULL));
 
-    // Initalize semaphore to -1 (so all threads will block by default)
-    // 0 - don't share semaphore with forked processes????
-    sem_init(&semaphore, 0, 0);
-    
-    pthread_create(&my_thread1, NULL, handleOne); 
-    pthread_create(&my_thread2, NULL, handleTwo); 
-    pthread_create(&my_thread3, NULL, handleThree); 
-    
     while(1) {
-        sem_getvalue(&semaphore, &num);
-        printf("Currently waiting: %d \n", num);
-        sleep(2);
-        sem_post(&semaphore);
+
     }
-    
+
+	printf("Welcome to the QNX Momentics IDE\n");
+	return EXIT_SUCCESS;
 }
